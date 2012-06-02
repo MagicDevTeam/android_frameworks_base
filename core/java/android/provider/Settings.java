69e187fb228eb90c5f85d6a6592631f4daa20593
Shareef Ali <shareefalis@gmail.com>
2012-06-01 22:35:45 -0500

    Phone: Sprint MWI Quirk: Phantom message wait indicator workaround (2/2)
    Part (1/2) http://review.cyanogenmod.com/#/c/13706/
    
    Change-Id: Iec65915475e6b8224b82feec5571f7bbded0545a
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2250,6 +2250,12 @@ public final class Settings {
          */
         public static final String MVNO_ROAMING = "mvno_roaming";
         
+
+        /** Sprint MWI Quirk: Show message wait indicator notifications
+         * @hide
+         */
+        public static final String ENABLE_MWI_NOTIFICATION = "enable_mwi_notification";
+
         /**
          * Show pointer location on screen?
          * 0 = no
