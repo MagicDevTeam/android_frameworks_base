a15086070af3d9cd4b3a03f855ef19b4803da424
athurh <athurh@gmail.com>
2013-08-15 15:37:21 +0200

    Fix hardware home key behavior on wake-up
    Change-Id: Ib489e4b8bb1d10591afc96c680c7865d61c959a0

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2102,7 +2102,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             // If we have released the home key, and didn't do anything else
             // while it was pressed, then it is time to go home!
-            if (!down) {
+            if (!down && mHomePressed) {
                 cancelPreloadRecentApps();
 
                 mHomePressed = false;
@@ -2165,7 +2165,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             // Remember that home is pressed and handle special actions.
             if (repeatCount == 0) {
-                mHomePressed = true;
+                if (down) {
+                    mHomePressed = true;
+                }
                 if (mHomeDoubleTapPending) {
                     mHomeDoubleTapPending = false;
                     mHandler.removeCallbacks(mHomeDoubleTapTimeoutRunnable);
