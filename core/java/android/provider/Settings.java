68950f9ee07dd1007302e718e5f277d169105535
0xD34D <clark@scheffsblend.com>
2013-08-17 16:13:21 -0700

    Add network usage stats to status bar. [1/2]
    PS2:
    Get the time delta between updates and use that to show
    average bytes per second.
    
    Change-Id: Ib06a713e6d93649ae8c8e5be5fd62c4f44a48416
    
    PS3:
    Code cleanup, change default text color and size to fit
    AOKP and add ability to change text color.
    
    PS4:
    Fix up layout for cleaner spacing and add for tablets.
    
    Change-Id: Ie3cdb038fecfe8cbb9b7a670c50dc7ebd22690e1
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/styles.xml
    
    clean white space
    
    NetStats: Don't update stats when screen is off
    
    Also moved registering of broadcast receiver and settings
    observer to onAttachedToWindow() and unregistering to
    onDetachedFromWindow() to avoid these being leaked.
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkStatsView.java
    
    Change-Id: I077515a0550a8268b2ec4ed09b5bf0f42315733d
    
    Network stats: use number formating based on android code
    
    Uses logic from Formatter.formatFileSize
    
    Change-Id: I9008df0e90fbaf8d733cfd853dc1ef6160cf67f5
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>
    
    make net work state show by default

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2719,7 +2719,7 @@ public final class Settings {
          */
         public static final String STATUS_BAR_SIGNAL_TEXT = "status_bar_signal";
 
-         /**
+        /**
          * Whether to control brightness from status bar
          *
          * @hide
@@ -2731,6 +2731,25 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
+
+        /**
+         * Whether to show the network status in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS = "status_bar_network_stats";
+
+        /**
+         * Frequency at which stats are updated, in milliseconds
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";
+
+        /**
+         * statusbar network stats text color
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS_TEXT_COLOR = "status_bar_network_stats_text_color";
 
         /**
          * Whether to use a separate delay for "slide to unlock" and security
