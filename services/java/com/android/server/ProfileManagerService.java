39226a7f864bf4520c9be7abd3d58a2d6b62a922
Danny Baumann <dannybaumann@web.de>
2013-09-02 15:50:07 +0200

    Move broadcast actions from service to ProfileManager so all API is

--- services/java/com/android/server/ProfileManagerService.java
+++ services/java/com/android/server/ProfileManagerService.java
@@ -55,15 +55,6 @@ public class ProfileManagerService extends IProfileManager.Stub {
     // Enable the below for detailed logging of this class
     private static final boolean LOCAL_LOGV = false;
 
-    /**
-    * <p>Broadcast Action: Current profile has been updated. This is triggered every time the
-    * currently active profile is updated, instead of selected.</p>
-    * <p> For instance, this includes profile updates caused by a locale change, which doesn't
-    * trigger a profile selection, but causes its name to change.</p>
-    * @hide
-    */
-    public static final String INTENT_ACTION_PROFILE_UPDATED = "android.intent.action.PROFILE_UPDATED";
-
     public static final String PERMISSION_CHANGE_SETTINGS = "android.permission.WRITE_SETTINGS";
 
     /* package */ static final File PROFILE_FILE =
@@ -203,16 +194,20 @@ public class ProfileManagerService extends IProfileManager.Stub {
         Log.d(TAG, "Set active profile to: " + newActiveProfile.getUuid().toString()
                 + " - " + newActiveProfile.getName());
 
-        Profile lastProfile = mActiveProfile;
-        mActiveProfile = newActiveProfile;
-        mDirty = true;
+                // Notify other applications of newly selected profile.
+                Intent broadcast = new Intent(ProfileManager.INTENT_ACTION_PROFILE_SELECTED);
+                broadcast.putExtra("name", mActiveProfile.getName());
+                broadcast.putExtra("uuid", mActiveProfile.getUuid().toString());
+                broadcast.putExtra("lastName", lastProfile.getName());
+                broadcast.putExtra("lastUuid", lastProfile.getUuid().toString());
+                mContext.sendBroadcastAsUser(broadcast, UserHandle.ALL);
 
                 restoreCallingIdentity(token);
                 persistIfDirty();
             } else if (lastProfile != mActiveProfile) {
                 // Something definitely changed: notify.
                 long token = clearCallingIdentity();
-                Intent broadcast = new Intent(INTENT_ACTION_PROFILE_UPDATED);
+                Intent broadcast = new Intent(ProfileManager.INTENT_ACTION_PROFILE_UPDATED);
                 broadcast.putExtra("name", mActiveProfile.getName());
                 broadcast.putExtra("uuid", mActiveProfile.getUuid().toString());
                 mContext.sendBroadcastAsUser(broadcast, UserHandle.ALL);
