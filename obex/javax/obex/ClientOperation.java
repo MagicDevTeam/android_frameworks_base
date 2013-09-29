3ca2eba650b716bbe762464b959ec7a4bf204176
SunRain <5908183@163.com>
2013-09-29 16:02:14 +0800

    Squashed changes for obex from cyangenmod
    These changes fixes the packages/apps/BluetoothExt build break
    
    commit 476f7c748eabc03117d5046ec42de9a701b912a0
    Merge: a2b017c 325bfe0
    Author: SunRain <5908183@163.com>
    Date:   Sun Sep 29 15:40:12 2013 +0800
    
        Squash changes for obex from cyangenmod
        commits are
        https://github.com/CyanogenMod/android_frameworks_base/commit/0e5fdcffdd1c3f1292a7cfa04660c966f70c883a
        https://github.com/CyanogenMod/android_frameworks_base/commit/9eaef385f367fe1d05444dc9ac33151fd8254aa6
    
    commit 325bfe0f00807729d7e3b1156238898e32808bdf
    Author: SunRain <5908183@163.com>
    Date:   Sun Sep 29 15:38:53 2013 +0800
    
        fix merge
    
    commit 0fead555c7cb4834d068124ba3708890859320ab
    Author: Steve Kondik <shade@chemlab.org>
    Date:   Sat Sep 7 09:11:15 2013 -0700
    
        Revert "Ported: Bluetooth Message Access Profile (MAP) from CM9"
    
        This reverts commit 0e5fdcffdd1c3f1292a7cfa04660c966f70c883a.
    
        MAP: Support MAP Server role on Bluedroid with javax.OBEX lib.
    
        Handle No END OF BODY for MAP 1.0 Server Get request. Only the
        last packet need send the 0x49(End of Body). For intermediate
        packets, we need to send 0x48 (Body).
    
        Move definitions for MAP or FTP Service Specific UUID and
        Request type for CONNECTION_ACCESS_REQUEST intent to
        BluetoothExt APK.
    
        Change-Id: I04cfeb8d53b8f8c19160a2de1eb448a7ec1f2c41
        CRs-fixed: 504042
    
    commit ed02b0cd1db1156d1e3892e3d906caf027d134df
    Author: Corey Garst <corey.garst@gmail.com>
    Date:   Wed Sep 12 21:41:17 2012 -0400
    
        Ported: Bluetooth Message Access Profile (MAP) from CM9
    
        This was added to CM10 to restore message exchange capabilities with automotive systems.
    
        Change-Id: Ib872bebc5c292b1f84c903b068e59b7eeca6d76f
    
        Conflicts:
        	obex/javax/obex/ClientOperation.java

--- obex/javax/obex/ClientOperation.java
+++ obex/javax/obex/ClientOperation.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2010-2011, Motorola, Inc.
+ * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2008-2009, Motorola, Inc.
  *
  * All rights reserved.
  *
@@ -124,21 +125,6 @@ public final class ClientOperation implements Operation, BaseStream {
                     (header).mAuthResp.length);
 
         }
-        if ((header).mConnectionID != null) {
-            mRequestHeader.mConnectionID = new byte[4];
-            System.arraycopy((header).mConnectionID, 0, mRequestHeader.mConnectionID, 0,
-                    4);
-
-        }
-    }
-
-    /**
-     * Allows to set flag which will force GET to be always sent as single packet request with
-     * final flag set. This is to improve compatibility with some profiles, i.e. PBAP which
-     * require requests to be sent this way.
-     */
-    public void setGetFinalFlag(boolean flag) {
-        mGetFinalFlag = flag;
     }
 
     /**
@@ -769,7 +755,6 @@ public final class ClientOperation implements Operation, BaseStream {
             }
         }
     }
-
     public void noEndofBody() {
 
     }
