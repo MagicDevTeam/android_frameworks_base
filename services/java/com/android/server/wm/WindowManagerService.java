6cb7ad3b6a9ce72d0af2457d3ec5a6ddde71c8da
Lars Greiss <kufikugel@googlemail.com>
2013-12-01 11:00:18 +0100

    fb: post time-critical systemui flags to window manager
    thanks Kiril for this suggestion. We are already using window
    manager for other things in immersive mode....so nice addition
    to fix a race condition
    
    Change-Id: I5d818214d7864014fa77c0d562d2908110d93303

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -10912,4 +10912,10 @@ public class WindowManagerService extends IWindowManager.Stub
         return mPolicy.getCurrentNavigationBarSize();
     }
 
+    /* @hide */
+    @Override
+    public void addSystemUIVisibilityFlag(int flag) {
+        mLastStatusBarVisibility |= flag;
+    }
+
 }
