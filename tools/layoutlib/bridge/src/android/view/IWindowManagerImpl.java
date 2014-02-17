073e8753a12eddbe6b0e7f22c1265f7514ebad4a
SunRain <41245110@qq.com>
2014-02-17 21:15:20 +0800

    Fix SDK compile
    Change-Id: I56c3021320646efcac8ca6f24c11d889cf070d2f

--- tools/layoutlib/bridge/src/android/view/IWindowManagerImpl.java
+++ tools/layoutlib/bridge/src/android/view/IWindowManagerImpl.java
@@ -20,6 +20,7 @@ import android.graphics.Point;
 import com.android.internal.view.IInputContext;
 import com.android.internal.view.IInputMethodClient;
 
+import android.content.Intent;
 import android.content.res.CompatibilityInfo;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
@@ -507,15 +508,18 @@ public class IWindowManagerImpl implements IWindowManager {
     /* @hide */
     @Override
     public boolean expandedDesktopHidesNavigationBar() {
+        return false;
     }
 
     /* @hide */
     @Override
     public boolean expandedDesktopHidesStatusBar() {
+        return false;
     }
 
     /* @hide */
     @Override
     public int getCurrentNavigationBarSize() {
+        return 0;
     }
 }
